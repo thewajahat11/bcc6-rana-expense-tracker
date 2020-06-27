8255c803dce5147ed3f11b4b493d47d0a61c18c7
Rana Wajahat <66208334+thewajahat11@users.noreply.github.com>
2020-06-27 18:04:46 +0500

    add build

---
+++ build/precache-manifest.9b97b0fe0f3e1aabf470974dd7cc423c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a9329747b12af2f65d203fc89139046",
+    "url": "/index.html"
+  },
+  {
+    "revision": "443fc2297050ffd4e7cd",
+    "url": "/static/css/main.cfd47f23.chunk.css"
+  },
+  {
+    "revision": "3db56f4ae4f39201d98f",
+    "url": "/static/js/2.7e0010b8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7e0010b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "443fc2297050ffd4e7cd",
+    "url": "/static/js/main.6418765c.chunk.js"
+  },
+  {
+    "revision": "6731f5be76a0aeeb5b32",
+    "url": "/static/js/runtime-main.16de008d.js"
+  },
+  {
+    "revision": "61c8a3af56a924aedd53ecad5130bc4c",
+    "url": "/static/media/img1.61c8a3af.jpg"
+  }
+]);
